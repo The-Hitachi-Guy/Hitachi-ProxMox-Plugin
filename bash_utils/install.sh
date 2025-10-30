26a2bcfc5fe30d3a7d3e76931cfd70b214dda8c3
wre136 <wre136@hotmail.com>
2025-10-30 13:07:11 -0500

    Added functions to install.sh

--- bash_utils/install.sh
+++ bash_utils/install.sh
@@ -16,6 +16,49 @@ make_gpt_partition() {
     kpartx -a "$disk-part1"
 }
 
+list_disks() {
+    # List discovered volume(s) and ask if reboot is needed
+    echo "#######################################"
+    echo "# Listing current disks of the system #"
+    echo "#######################################"
+    lsblk -o NAME,MODEL,SIZE,WWN
+    echo
+}
+
+verify_disks_found() {
+    disksVerified="N"
+    while [[ "$disksVerified" != "Y" && "$disksVerified" != "y" ]]; do
+        list_disks
+        read -p "Are SAN volume(s) found? (Y/N): " foundVolumes
+        if [ "$foundVolumes" == "Y" ] || [ "$foundVolumes" == "y" ]; then
+            disksVerified="Y"
+        else
+            read -p "Do you want to rescan SCSI bus to find volumes? (Y/N): " rescanDisks
+            if [ "$rescanDisks" == "Y" ] || [ "$rescanDisks" == "y" ]; then
+                rescan-disks
+            else
+                read -p "Do you want to reboot this system to find volumes? (Y/N): " confirmReboot
+                if [ "$confirmReboot" == "Y" ] || [ "$confirmReboot" == "y" ]; then
+                    reboot
+                else
+                    echo "Exiting install now..."
+                    exit 1
+                fi
+            fi
+        fi
+    done
+    return 1
+}
+
+rescan-disks() {
+    # Rescan SCSI Bus to detect the new volume(s)
+    echo "#########################################"
+    echo "# Rescanning SCSI Bus to find new disks #"
+    echo "#########################################"
+    rescan-scsi-bus.sh --largelun --multipath --issue-lip-wait=10 --alltargets
+    echo
+}
+
 currentNodeName=$(hostname)
 isClusterNode="N"
 echo "Conifugure this system a standalone server or as a cluster node?"
@@ -93,31 +136,32 @@ echo
 # Waiting for user to zone and attached FC volumes
 read -p "Hit enter to continue once the volumes are attached to the server..."
 echo
+verify_disks_found
 
-# Rescan SCSI Bus to detect the new volume(s)
-echo "#########################################"
-echo "# Rescanning SCSI Bus to find new disks #"
-echo "#########################################"
-rescan-scsi-bus.sh -i && rescan-scsi-bus.sh -a
-echo
+# # Rescan SCSI Bus to detect the new volume(s)
+# echo "#########################################"
+# echo "# Rescanning SCSI Bus to find new disks #"
+# echo "#########################################"
+# rescan-scsi-bus.sh -i && rescan-scsi-bus.sh -a
+# echo
 
-# List discovered volume(s) and ask if reboot is needed
-echo "#######################################"
-echo "# Listing current disks of the system #"
-echo "#######################################"
-lsblk
-echo
-read -p "Are SAN volume(s) found? (Y/N): " foundVolumes
-if [ "$foundVolumes" != "Y" ] && [ "$foundVolumes" != "y" ]; then
-	read -p "Do you want to reboot this system to find volumes? (Y/N): " confirmReboot
-	if [ "$confirmReboot" == "Y" ] || [ "$confirmReboot" == "y" ]; then
-		reboot
-	else
-		echo "Exiting install now..."
-		exit 1
-    fi
-fi
-echo
+# # List discovered volume(s) and ask if reboot is needed
+# echo "#######################################"
+# echo "# Listing current disks of the system #"
+# echo "#######################################"
+# lsblk
+# echo
+# read -p "Are SAN volume(s) found? (Y/N): " foundVolumes
+# if [ "$foundVolumes" != "Y" ] && [ "$foundVolumes" != "y" ]; then
+# 	read -p "Do you want to reboot this system to find volumes? (Y/N): " confirmReboot
+# 	if [ "$confirmReboot" == "Y" ] || [ "$confirmReboot" == "y" ]; then
+# 		reboot
+# 	else
+# 		echo "Exiting install now..."
+# 		exit 1
+#     fi
+# fi
+# echo
 
 # Creating list of Hitachi SAN Disks and Non-Hitachi Disks on the system
 echo "#########################################################################"
